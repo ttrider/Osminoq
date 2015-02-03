fba6d48c00e720234600f13e850474c6617680a6
Vladimir Yangurskiy <vladyang@microsoft.com>
2015-02-02 17:46:59 -0800

    tests

---
+++ Tests/CoreTests/CsvTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TTRider.Osminoq.CoreTests
+{
+    [TestClass]
+    public class CsvTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
