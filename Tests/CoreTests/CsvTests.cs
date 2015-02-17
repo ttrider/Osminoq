127dc5e76f501c9c27a8fdd47903df40362c2f02
ttrider <vyangurs@hotmail.com>
2015-02-17 09:23:50 -0800

    CSV core objects

--- Tests/CoreTests/CsvTests.cs
+++ Tests/CoreTests/CsvTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TTRider.Osminoq.CoreTests
 {
@@ -6,8 +7,58 @@ namespace TTRider.Osminoq.CoreTests
     public class CsvTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void StringConverterTest00()
         {
+            const string val = "$123.43";
+            var pattern = new Regex(@"\$(?<value>.*)", RegexOptions.Compiled);
+            Assert.AreEqual("123.43", FieldExtractor.ToString(val, pattern));
+        }
+
+        [TestMethod]
+        public void StringConverterTest01()
+        {
+            const string val = "123.43";
+            var pattern = new Regex(@"\$(?<value>.*)", RegexOptions.Compiled);
+            Assert.IsNull(FieldExtractor.ToString(val, pattern));
+        }
+
+        [TestMethod]
+        public void StringConverterTest02()
+        {
+            const string val = "$123.43";
+            var pattern = new Regex(@"\$(?<valuew>.*)", RegexOptions.Compiled);
+            Assert.IsNull(FieldExtractor.ToString(val, pattern));
+        }
+
+        [TestMethod]
+        public void StringConverterTest03()
+        {
+            const string val = "123.43";
+            Assert.AreEqual("123.43", FieldExtractor.ToString(val, null));
+        }
+
+
+        [TestMethod]
+        public void IntConverterTest00()
+        {
+            const string val = "$123";
+            var pattern = new Regex(@"\$(?<value>.*)", RegexOptions.Compiled);
+            Assert.AreEqual(123, FieldExtractor.ToInt(val, pattern));
+        }
+
+        [TestMethod]
+        public void IntConverterTest01()
+        {
+            const string val = "123.43";
+            var pattern = new Regex(@"\$(?<value>.*)", RegexOptions.Compiled);
+            Assert.IsNull(FieldExtractor.ToInt(val, pattern));
+        }
+
+        [TestMethod]
+        public void IntConverterTest02()
+        {
+            const string val = "123";
+            Assert.AreEqual(123, FieldExtractor.ToInt(val, null));
         }
     }
 }
