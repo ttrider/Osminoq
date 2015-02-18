dd38747ae64d367959b5ba43c848ec16a10db628
ttrider <vyangurs@hotmail.com>
2015-02-17 16:07:04 -0800

    missing files

---
+++ Osminoq/DataType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTRider.Osminoq
+{
+    public enum DataType
+    {
+        String,
+        Integer,
+        Double,
+        Boolean,
+        DateTime,
+
+        Guid
+    }
+}
